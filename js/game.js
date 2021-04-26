fff237a8eed2ffd44b220309232b0a9fe0f50ec8
Dimitrii Vinogradov <siteddv@gmail.com>
2021-04-26 17:53:04 +0600

    changed touchscreen support

--- js/game.js
+++ js/game.js
@@ -27,7 +27,7 @@ fallAudio.src = "audio/fall.mp3";
 
 document.addEventListener("keydown", keyDown);
 document.addEventListener("mousedown", keyDown);
-document.addEventListener("touchstart", keyDown);
+document.addEventListener("touchstart", keyDown, false);
 
 function keyDown() {
    yBirdPosition -= 40;
